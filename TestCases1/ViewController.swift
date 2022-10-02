a44bb3cee7c2052aaf5ee9cc90c167211cb12154
AI1681 <Harshit.mahajan@appinventiv.com>
2022-10-02 17:12:46 +0530

    new changes

--- TestCases1/ViewController.swift
+++ TestCases1/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        //////// what have we done 
+        //////// what have
         let ferrari = CarsWay(type: .sport, transmissionMode: .drive)
         ferrari.start(minutes: 120)
         print(ferrari.miles)
